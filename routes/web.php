aef0d4baa4074b28d6aa5ed325e8f1c72d0c9674
Amir-Syaifudin <clanads3@gmail.com>
2026-09-23 14:49:32 +0700

    Add diagnostic route for environment detection

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::post('/upload', [VulnUploadController::class, 'store']);
 // CVE-2026-48019: CRLF injection via validateEmail()
 Route::get('/contact', [VulnContactController::class, 'form']);
 Route::post('/contact', [VulnContactController::class, 'send']);
+
+// CVE-2024-52301: diagnostic route to visualize environment detection flip
+Route::get('/env-check', fn () => 'Current environment: '.app()->environment());
